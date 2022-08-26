0a4e4bb7da43327658d2d353341fb23802283998
thunder609 <oswaldo-pachco@hotmail.es>
2022-08-26 01:42:28 -0400

    opetativo

---
+++ contact/forms.py
@@ -0,0 +1,13 @@
+from tkinter import Widget
+from xml.dom.minidom import Attr
+from django.forms import ModelForm
+from .models import Contact
+
+class ContactForm(ModelForm):
+    class Meta:
+        model =Contact
+        exclude =('date',)
+
+
+  
+ 
---
+++ contact/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+     path('',views.index,name='contact'),
+     path('view/<int:id>',views.view ,name='contact_view'),
+     path('edit/<int:id>',views.edit, name='contact_edit'),
+     path('create/',views.create,name='contact_create'),
+     path('delete/<int:id>',views.delete,name="contact_delete")
+]
